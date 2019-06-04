9ec5036ad2571350ff50eb67a931fc181bd2ccfc
jeonghun.kim <jeonghun.kim@kurlycorp.com>
2019-06-04 13:33:01 +0900

    store 추가

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,16 +1,11 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import PersonStore from './PersonStore';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  },
+  modules: {
+    PersonStore,
+  }
 });
